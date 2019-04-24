6553cead446b8c1e16c86ee74e3d1fddd0b83b20
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2019-04-24 17:49:33 +0500

    Added tags

---
+++ start/hooks.js
@@ -0,0 +1,13 @@
+
+const { hooks } = require('@adonisjs/ignitor')
+
+hooks.after.providersBooted(() => {
+  const View = use('View')
+  const Tag = use('App/Models/Tag')
+
+  Tag.all().then((tags) => {
+    View.global('tags', tags)
+  }).catch(() => {
+
+  })
+})
