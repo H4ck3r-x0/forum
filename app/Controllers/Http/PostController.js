fe862938ffed61fe394c5da634db5e351bb07890
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2019-04-29 20:19:09 +0500

    added Best answer to the posts.

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -12,6 +12,8 @@ class PostController {
             .with('tag')
             .with('replies')
             .with('replies.user')
+            .with('answer')
+            .with('answer.user')
             .where('slug', params.slug)
             .firstOrFail()
 
